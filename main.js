2b24ba39193dc3e07051d3b6e75e2b035bb0e2d2
Joey <joeybb@MacBook-Pro.local>
2017-04-17 15:02:34 -0500

    first commit, javascript not complete

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("why won't you just make a stupid tree?");
+
+// var character = document.getElementById('input').value;
+// var height = document.getElementById('height').value;
+// var button = document.getElementById('btn')
+// var userInput = [character += height]
+
+// // button.onclick = function() {
+// // 	console.log(" ");
+// // };
+
+// function tree (growTree) {
+// 	for (i = 0; i < usernput.length; i++) {
+// 		tree += userInput[i];
+// }
+
+var input = document.getElementById("input").value;
+var height = document.getElementById("height").value;
+var submit = document.getElementById("submit").addEventListener("click", getHeight);
+var clear = document.getElementById("clear");
+
+
+function getHeight (height){
+parseInt(height);
+};
+
+function tree (growTree){
+};
+
+if (input === " , "){
+	alert("Both Fields Must be Complete!");
+} else (height === " , ")
+	alert("Both Fields Must be Complete!");
